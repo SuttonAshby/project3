734b86ea00b641a082b3efbfef92dc0fe1259ce7
Handrew <andrew.a.han@gmail.com>
2018-06-25 17:16:53 -0400

    branching

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Cool</Text>
+        <Text>Awesome</Text>
       </View>
     );
   }
