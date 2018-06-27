69083d0f6c301c7bb0b0971e25517d0c7fc929fe
Joseph Holder <josephholder@Josephs-MacBook-Pro.local>
2018-06-26 21:28:58 -0400

    login button start

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,34 +11,57 @@ import Container from '../components/Container';
 import Button from '../components/Button';
 import Label from '../components/Label';
 
-const Login = () => (
-    <ScrollView style={styles.scroll}>
-        <Container>
-            <Label text="Username or Email" />
-            <TextInput
-                style={styles.textInput}
-            />
-        </Container>
-        <Container>
-            <Label text="Password" />
-            <TextInput
-                secureTextEntry={true}
-                style={styles.textInput}
-            />
-        </Container>
-        <Container>
-            <Button
-                label="Register"
-                styles={{ button: styles.alignRight, label: styles.label }}
-            />
-            <Button
-                label="Login"
-                styles={{ button: styles.alignRight, label: styles.label }}
-            />
-        </Container>
-    </ScrollView>
-);
+class Login extends Component {
 
+    state = {
+        username: "",
+        password: ""
+      };
+
+    loginSubmit = event => {
+        event.preventDefault();
+        console.log(`Hello Eric ${this.state.username} ${this.state.password}`);
+    };
+
+    render() {
+        return (
+
+            <ScrollView style={styles.scroll}>
+                <Container>
+                    <Label text="Username" />
+                    <TextInput
+                        style={styles.textInput}
+                        name="username"
+                        onChangeText={(text) => this.setState({text})}
+                        type="text"
+                        value={this.state.username}
+                    />
+                </Container>
+                <Container>
+                    <Label text="Password" />
+                    <TextInput
+                        secureTextEntry={true}
+                        name="password"
+                        style={styles.textInput}
+                        onChangeText={(text) => this.setState({text})}
+                        value={this.state.password}
+                    />
+                </Container>
+                <Container>
+                    <Button
+                        label="Register"
+                        styles={{ button: styles.alignRight, label: styles.label }}
+                    />
+                    <Button
+                        label="Login"
+                        styles={{ button: styles.alignRight, label: styles.label }}
+                        onPress={this.loginSubmit}
+                    />
+                </Container>
+            </ScrollView>
+        );
+    }
+}
 const styles = StyleSheet.create({
     scroll: {
         backgroundColor: '#E1D7D8',
