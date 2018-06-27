955836aa56de6615834d29b5e5bd330a0a4837ac
Sutton Ashby <korim_fidelis@yahoo.com>
2018-06-27 14:37:50 -0400

    building grid

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import React from 'react';
-<<<<<<< HEAD
-import { StyleSheet, Text, View } from 'react-native';
-=======
 import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {
   StackNavigator,
 } from 'react-navigation';
 import Login from './src/pages/Login';
+import Home from './src/pages/Home';
 
->>>>>>> 4c413fb40739ce2e33c00157dc6e48c40a674e6f
 
 export default class App extends React.Component {
   render() {
     return (
-      <Login />
+      <Home />
         // <View>
         // <Text>Open up App.js to start working on your app!</Text>
         // <Text>Changes you make will automatically reload.</Text>
