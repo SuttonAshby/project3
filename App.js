4ac2f1e9694754f656d0bcc9fe85ef911ae87d0c
Handrew <andrew.a.han@gmail.com>
2018-06-27 20:22:57 -0400

    switched to incoming changes

--- App.js
+++ App.js
@@ -11,25 +11,18 @@ import Home from './src/pages/Home';
 export default class App extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Awesome</Text>
-      </View>
-=======
-      <Login />
-=======
-      <Home />
->>>>>>> 7017154ef79b5592608e216492446f3c1b232766
-        // <View>
-        // <Text>Open up App.js to start working on your app!</Text>
-        // <Text>Changes you make will automatically reload.</Text>
-        // <Text>Shake your phone to open the developer menu.</Text>
-        // <Text>Joseph Holder</Text> */}
-        // </View>
->>>>>>> 4c413fb40739ce2e33c00157dc6e48c40a674e6f
+      <div>
+        <Login />
+
+        <Home />
+        { // <View>
+          // <Text>Open up App.js to start working on your app!</Text>
+          // <Text>Changes you make will automatically reload.</Text>
+          // <Text>Shake your phone to open the developer menu.</Text>
+          // <Text>Joseph Holder</Text> */}
+          // </View>
+        }
+      </div>
     );
   }
 }
