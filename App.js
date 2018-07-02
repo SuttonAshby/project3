44a720f6446ab0f2a83fe264bb422134cd0f8df9
Sutton Ashby <korim_fidelis@yahoo.com>
2018-07-02 15:54:45 -0400

    moved text origin to state

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-<<<<<<< HEAD
 import { createStackNavigator } from 'react-navigation';
 import Login from './src/screens/Login';
 import Home from './src/screens/Home';
 import Register from './src/screens/Register';
-=======
-import {
-  StackNavigator,
-} from 'react-navigation';
-import Login from './src/pages/Login';
-import Home from './src/pages/Home';
 import CameraView from './src/pages/CameraView';
 import ImagePickerTest from './src/pages/ImagePickerTest';
->>>>>>> master
 
 const RootStack = createStackNavigator(
   {
@@ -23,13 +15,12 @@ const RootStack = createStackNavigator(
     RegisterScreen: Register,
   },
   {
-    initialRouteName: 'RegisterScreen',
+    initialRouteName: 'HomeScreen',
   }
 );
 
 export default class App extends React.Component {
   render() {
-<<<<<<< HEAD
     return <RootStack />;
     // <View>
     // <Text>Open up App.js to start working on your app!</Text>
@@ -37,10 +28,5 @@ export default class App extends React.Component {
     // <Text>Shake your phone to open the developer menu.</Text>
     // <Text>Joseph Holder</Text> */}
     // </View>
-=======
-    return (
-        <Home />
-    );
->>>>>>> master
   }
 }
