d513af303386f281ef941749f01ead0d01251bc0
Sutton Ashby <korim_fidelis@yahoo.com>
2018-07-01 23:26:00 -0400

    refactoring grid, adding modal and board state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
 
 import {
-<<<<<<< HEAD
-    Modal,
-    TouchableHighlight,
+    Button,
     StyleSheet,
+    CameraRoll,
     Text,
     View,
+    Image,
+    TouchableOpacity,
     TextInput,
-    ScrollView
-=======
-  Button,
-  StyleSheet,
-  CameraRoll,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  TextInput,
-  ScrollView
->>>>>>> 589aba374d89536d61228c688ce911d02f3c0da6
+    ScrollView,
+    Modal,
+    TouchableHighlight
 } from 'react-native';
-import { Camera, Permissions } from 'expo'
-
 
 import { Camera, FileSystem, Permissions, Constants, takeSnapshotAsync, ImagePicker } from 'expo';
 import Square from "../components/square";
@@ -31,86 +21,168 @@ import anime from "../../anime.json";
 // =================================================================
 
 export default class Home extends Component {
-<<<<<<< HEAD
+
     state = {
         hasCameraPermission: null,
-        type: Camera.Constants.Type.back,
+        type: Camera.Constants.Type.front,
+        cameraRollUri: null,
+        // image: null,
+        
+        // source: null,
+        anime,
         modalVisible: false,
-        photo: null
     };
 
-    setModalVisible(visible) {
-        this.setState({ modalVisible: visible });
+    askPermissionsAsync = async () => { // this was needed to get image picker working and this has to be called in the method as well
+        await Permissions.askAsync(Permissions.CAMERA);
+        await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        // you would probably do something to verify that permissions
+        // are actually granted, but I'm skipping that for brevity
+    };
+
+
+    // =================================================================
+    componentWillMount = async () => {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ hasCameraPermission: status === 'granted' });
     }
+    // =================================================================
+
+
+    _saveToCameraRollAsync = async () => {
+        await this.askPermissionsAsync();
+        console.log("savetoCameraRoll");
+        let result = await takeSnapshotAsync(this._container, {
+            format: 'png',
+            result: 'file',
+        });
+
+        let saveResult = await CameraRoll.saveToCameraRoll(result, 'photo');
+        this.setState({ cameraRollUri: saveResult });
+    };
+    // =================================================================
+    // the method will allow the camera to take a picture
+    // snap = async () => {
+    //   console.log("snapped");
+    //   console.log(this.camera);
+    //   if (this.camera) {
+    //     console.log('Taking photo');
+    //     let photo = await this.camera.takePictureAsync();
+    //     console.log(photo);
+    // }
+    // };
+    // =================================================================
+    // _pickImage = async () => {
+    //   await this.askPermissionsAsync(); //need this permission to get the imagepicker working
+    //   console.log("ImagePicker");
+    //   let result = await ImagePicker.launchImageLibraryAsync({
+    //     allowsEditing: true,
+    //     aspect: [4, 3],
+    //   });
+
+    //   console.log(result);
+
+    //   if (!result.cancelled) {
+    //     this.setState({ image: result.uri });
+    //   }
+    // };
+    // =================================================================
+
+    //this works!!!
+    async press() {
+
+        console.log('Button Pressed');
+
+        console.log('Taking photo');
+        let photo = await this.camera.takePictureAsync({ base64: true, exif: true });
+
+        let saveResult = await CameraRoll.saveToCameraRoll(photo.uri, 'photo');
+        this.setState({ cameraRollUri: saveResult });
+        console.log("pressed");
+    }
+    // =================================================================
+
 
     render() {
-        return (
-            // Try setting `flexDirection` to `column`.
-            <View style={styles.container}>
-                <View style={styles.board}>
-                    <View style={style = styles.row}>
-                        <View style={{ flex: 1, backgroundColor: '#EE2C38' }} />
-                        <View style={{ flex: 1, backgroundColor: '#FAA030' }} />
-                        {/* <Camera style={{ flex: 1 }} type={this.state.type}>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}/></Camera> */}
-                        <View style={{ flex: 1, backgroundColor: '#32B76C' }} />
-                    </View>
-                    <View style={style = styles.row}>
-                        <View style={{ flex: 1, backgroundColor: '#32B76C' }} />
-                        <View style={{ flex: 1, backgroundColor: '#EE2C38' }} />
-                        <View style={{ flex: 1, backgroundColor: '#FAA030' }} />
-                        {/* <Camera style={{ flex: 1 }} type={this.state.type}>
-                            <View
-                                style={{
-                                    flex: 1,
-                                    backgroundColor: 'transparent',
-                                    flexDirection: 'row',
-                                }} /></Camera> */}
-                    </View>
-                    <View style={style = styles.row}>
-                        <View style={{ flex: 1, backgroundColor: '#EE2C38' }} />
 
 
-                        <View style={{ flex: 1, backgroundColor: '#FAA030' }} />
-                        {/* <View style={{ flex: 1, backgroundColor: '#32B76C' }} /> */}
-                        <TouchableHighlight style={{ flex: 1, backgroundColor: '#32B76C' }}
-                            onPress={() => {
-                                this.setModalVisible(true);
-                            }}>
-                            <Text></Text>
-                        </TouchableHighlight>
+        let { hasCameraPermission, image } = this.state;
+
+        if (hasCameraPermission === null) {
+            return <Text>Hello</Text>;
+        } else if (hasCameraPermission === false) {
+            return <Text>No access to camera</Text>;
+        } else {
+
+            return (
+
+
+
+                // Try setting `flexDirection` to `column`.
+                <View style={styles.container}
+                    collapsable={false}
+                    ref={view => {
+                        this._container = view;
+                    }} >
+                    <View style={styles.board}>
+                        <View style={styles.row}>
+                            <View style={{ flex: 1, backgroundColor: '#EE2C38' }} />
+                            <View style={{ flex: 1, backgroundColor: '#FAA030' }} />
+                            <View style={{ flex: 1, backgroundColor: '#32B76C' }} />
+                        </View>
+                        <View style={styles.row}>
+                            <View style={{ flex: 1, backgroundColor: '#32B76C' }} />
+                            {this.state.cameraRollUri ?
+                                <Image
+                                    source={{ uri: this.state.cameraRollUri }}
+                                    style={{ flex: 1 }}
+                                /> : <View style={{ flex: 1, backgroundColor: '#EE2C38' }} />}
+                            <View style={{ flex: 1, backgroundColor: '#FAA030' }} />
+                        </View>
+                        <View style={styles.row}>
+                            <Camera
+                                style={{ flex: 1 }}
+                                ref={(ref) => { this.camera = ref }}
+                            >
+                                <View style={{ flex: 1 }}></View>
+
+                            </Camera>
+                            <View style={{ flex: 1, backgroundColor: '#FAA030' }} ><Text>save To Camera Roll Async</Text></View>
+                            <View style={{ flex: 1, backgroundColor: '#32B76C' }} >
+                                <Text>ImageClicker</Text>
+                                <Button
+                                    title="Click On Me"
+                                    style={{ flex: 0, backgroundColor: 'red' }}
+                                    onPress={this.press.bind(this)}
+                                />
+                            </View>
+                        </View>
                     </View>
-                </View>
-                <Modal
-                    animationType="fade"
-                    transparent={false}
-                    visible={this.state.modalVisible}
-                    onRequestClose={() => {
-                        alert('Modal has been closed.');
-                    }}>
-                    <View style={styles.container}>
-                        <View style={styles.board}>
-                            <Camera style={{ flex: 1 }} type={this.state.type} ref={ref => {
-                                this.camera = ref;
-                            }}>
-                                <View
-                                    style={{
-                                        flex: 1,
-                                        backgroundColor: 'transparent',
-                                        flexDirection: 'row',
-                                    }} />
-                                <TouchableHighlight
-                                    onPress={() => {
-                                        this.setModalVisible(!this.state.modalVisible);
-                                    }}>
-                                    <Text>Hide Modal</Text>
-                                </TouchableHighlight>
-                                {/* <TouchableHighlight
+                    <Modal
+                        animationType="fade"
+                        transparent={false}
+                        visible={this.state.modalVisible}
+                        onRequestClose={() => {
+                            alert('Modal has been closed.');
+                        }}>
+                        <View style={styles.container}>
+                            <View style={styles.board}>
+                                <Camera style={{ flex: 1 }} type={this.state.type} ref={ref => {
+                                    this.camera = ref;
+                                }}>
+                                    <View
+                                        style={{
+                                            flex: 1,
+                                            backgroundColor: 'transparent',
+                                            flexDirection: 'row',
+                                        }} />
+                                    <TouchableHighlight
+                                        onPress={() => {
+                                            this.setModalVisible(!this.state.modalVisible);
+                                        }}>
+                                        <Text>Hide Modal</Text>
+                                    </TouchableHighlight>
+                                    {/* <TouchableHighlight
                                     onPress={() => () => {
                                         console.log("taking a picture")
                                         // if (this.camera) {
@@ -122,176 +194,21 @@ export default class Home extends Component {
                                     }}>
                                     <Text>Snap</Text>
                                 </TouchableHighlight> */}
-                            </Camera>
+                                </Camera>
 
+                            </View>
                         </View>
-                    </View>
-                </Modal >
-            </View >
+                    </Modal >
+                </View>
 
-        );
-=======
+            );
 
-  state = {
-    hasCameraPermission: null,
-    type: Camera.Constants.Type.front,
-    cameraRollUri: null,
-    image: null,
-    source: null,
-    anime,
-  };
-
-  askPermissionsAsync = async () => { // this was needed to get image picker working and this has to be called in the method as well
-    await Permissions.askAsync(Permissions.CAMERA);
-    await Permissions.askAsync(Permissions.CAMERA_ROLL);
-    // you would probably do something to verify that permissions
-    // are actually granted, but I'm skipping that for brevity
-  };
-
-
-  // =================================================================
-  componentWillMount = async () => {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
-  }
-  // =================================================================
-
-
-  _saveToCameraRollAsync = async () => {
-    await this.askPermissionsAsync();
-    console.log("savetoCameraRoll");
-    let result = await takeSnapshotAsync(this._container, {
-      format: 'png',
-      result: 'file',
-    });
-
-    let saveResult = await CameraRoll.saveToCameraRoll(result, 'photo');
-    this.setState({ cameraRollUri: saveResult });
-  };
-  // =================================================================
-  // the method will allow the camera to take a picture
-  // snap = async () => {
-  //   console.log("snapped");
-  //   console.log(this.camera);
-  //   if (this.camera) {
-  //     console.log('Taking photo');
-  //     let photo = await this.camera.takePictureAsync();
-  //     console.log(photo);
-  // }
-  // };
-  // =================================================================
-  // _pickImage = async () => {
-  //   await this.askPermissionsAsync(); //need this permission to get the imagepicker working
-  //   console.log("ImagePicker");
-  //   let result = await ImagePicker.launchImageLibraryAsync({
-  //     allowsEditing: true,
-  //     aspect: [4, 3],
-  //   });
-
-  //   console.log(result);
-
-  //   if (!result.cancelled) {
-  //     this.setState({ image: result.uri });
-  //   }
-  // };
-  // =================================================================
-
-  //this works!!!
-  async press() {
-
-    console.log('Button Pressed');
-
-    console.log('Taking photo');
-    let photo = await this.camera.takePictureAsync({ base64: true, exif: true });
-
-    let saveResult = await CameraRoll.saveToCameraRoll(photo.uri, 'photo');
-    this.setState({ cameraRollUri: saveResult });
-    console.log("pressed");
-  }
-  // =================================================================
-
-
-  render() {
-
-
-    let { hasCameraPermission, image } = this.state;
-
-    if (hasCameraPermission === null) {
-      return <Text>Hello</Text>;
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    } else {
-
-      return (
-
-
-
-        // Try setting `flexDirection` to `column`.
-        <View style={styles.container}
-          collapsable={false}
-          ref={view => {
-            this._container = view;
-          }} >
-
-          <View style={[{ flex: 1, flexDirection: 'row' }, styles.elementsContainer]}>
-            <View style={{ flex: 1, backgroundColor: '#EE2C38' }} />
-            <View style={{ flex: 1, backgroundColor: '#FAA030' }} />
-            <View style={{ flex: 1, backgroundColor: '#32B76C' }} />
-          </View>
-          <View style={[{ flex: 1, flexDirection: 'row' }, styles.elementsContainer]}>
-            <View style={{ flex: 1, backgroundColor: '#32B76C' }} />
-
-
-
-            {this.state.cameraRollUri ?
-              <Image
-                source={{ uri: this.state.cameraRollUri }}
-                style={{ flex: 1 }}
-              /> : <View style={{ flex: 1, backgroundColor: '#EE2C38' }} />}
-
-
-
-
-            <View style={{ flex: 1, backgroundColor: '#FAA030' }} />
-          </View>
-          <View style={[{ flex: 1, flexDirection: 'row' }, styles.elementsContainer]}>
-
-            <Camera
-              style={{ flex: 1 }}
-              ref={(ref) => { this.camera = ref }}
-            >
-              <View style={{ flex: 1 }}></View>
-
-            </Camera>
-
-            <View style={{ flex: 1, backgroundColor: '#FAA030' }} ><Text>save To Camera Roll Async</Text></View>
-
-            <View style={{ flex: 1, backgroundColor: '#32B76C' }} >
-
-
-              <Text>ImageClicker</Text>
-
-              <Button
-                title="Click On Me"
-                style={{ flex: 0, backgroundColor: 'red' }}
-                onPress={this.press.bind(this)}
-              />
-
-
-            </View>
-          </View>
-        </View> 
-
-      );
-
->>>>>>> 589aba374d89536d61228c688ce911d02f3c0da6
+        }
     }
-}
 
 }
 
 const styles = {
-<<<<<<< HEAD
     container: {
         marginTop: 48,
         //   width: 200,
@@ -299,8 +216,8 @@ const styles = {
         flexDirection: 'row',
         flex: 1,
         // aspectRatio: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        // justifyContent: 'center',
+        // alignItems: 'center',
 
     },
     board: {
@@ -318,38 +235,5 @@ const styles = {
         //   marginBottom: 24
     }
 }
-=======
-  container: {
-    marginTop: 48,
-    //   width: 200,
-    //   height: 200,
-    //   flexDirection: 'row',
-    flex: 0.7,
-    // aspectRatio: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    alignItems: 'center'
-  },
-  headerStyle: {
-    fontSize: 36,
-    textAlign: 'center',
-    fontWeight: '100',
-    marginBottom: 24
-  },
-  elementsContainer: {
-    backgroundColor: '#ecf5fd',
-    //   marginLeft: 24,
-    //   marginRight: 24,
-    //   marginBottom: 24
-  },
-  squareContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-end'
-  }
-}
 
-      // =================================================================
-
->>>>>>> 589aba374d89536d61228c688ce911d02f3c0da6
 
