72f064ea3774f13d6f440b1077158d2e890016c6
Sutton Ashby <korim_fidelis@yahoo.com>
2018-07-12 20:13:51 -0400

    before pull request

--- components/AppComponents/Square.js
+++ components/AppComponents/Square.js
@@ -7,40 +7,16 @@ import {
 
 export default class Square extends Component {
 
-    // render() {
-    //     return <TouchableHighlight onPress={this.props.onPress} style={[styles.emptySquare, { backgroundColor: this.props.color }]}>
-    //         {this.props.source ? <ImageBackground source={{ uri: this.props.source }}style={{ flex: 1 }} >
-    //         <Text>{this.props.name}</Text>
-    //         </ImageBackground> : <Text>{this.props.name}</Text>}
-    //     </TouchableHighlight>
-    // }
-
     render() {
-        return <TouchableHighlight onPress={this.props.onPress} 
-            style={!this.props.source ? [styles.emptySquare, { backgroundColor: this.props.color }] : styles.photoSquare }>
-            {this.props.source ? <ImageBackground source={{ uri: this.props.source }}style={{ flex: 1 }} >
-            <Text style={styles.text}>{this.props.name}</Text>
+        return <TouchableHighlight onPress={this.props.onPress}
+            style={!this.props.source ? [styles.emptySquare, { backgroundColor: this.props.color }] : styles.photoSquare}>
+            {this.props.source ? <ImageBackground source={{ uri: this.props.source }} style={{ flex: 1 }} >
+                <Text style={styles.text}>{this.props.name}</Text>
             </ImageBackground> : <Text>{this.props.name}</Text>}
         </TouchableHighlight>
     }
 
 }
-
-// return  this.props.source ? 
-// <TouchableHighlight style={styles.emptySquare}>
-//     <ImageBackground source={{ uri: this.props.source }}
-//     style={{ flex: 1 }} ><Text>{this.props.name}</Text></ImageBackground>
-// </TouchableHighlight> : 
-// <TouchableHighlight style={styles.photoSquare}>
-//     <Text>{this.props.name}</Text>
-// </TouchableHighlight>}
-
-
-// source
-// name
-// ID
-// functions
-//color
 
 const styles = {
     emptySquare: {
@@ -52,8 +28,8 @@ const styles = {
         flex: 1,
     },
     text: {
-        position: "absolute", 
-        bottom: 0, 
+        position: "absolute",
+        bottom: 0,
         right: 0,
         backgroundColor: "rgba(255, 255, 255, 0.3)"
     }
